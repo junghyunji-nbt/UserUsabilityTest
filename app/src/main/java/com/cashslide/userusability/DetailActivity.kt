7a3f8c6ce00cc373ab43c97cb989be9c7d0493cd
jung.hyunji <jung.hyunji@nbt.com>
2018-08-04 23:57:14 +0900

    디테일 화면

---
+++ app/src/main/java/com/cashslide/userusability/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.cashslide.userusability
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DetailActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+
+
+}
--- app/src/main/java/com/cashslide/userusability/adapter/HorizontalAdapter.kt
+++ app/src/main/java/com/cashslide/userusability/adapter/HorizontalAdapter.kt
@@ -1,12 +1,14 @@
 package com.cashslide.userusability.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.cashslide.userusability.DetailActivity
 import com.cashslide.userusability.R
 import kotlinx.android.synthetic.main.horizontal_list.view.*
 import kotlinx.android.synthetic.main.horizontal_list_dummy_item.view.*
@@ -79,6 +81,11 @@ class HorizontalAdapter(var context: Context) : RecyclerView.Adapter<RecyclerVie
                         .load(imageId)
                         .diskCacheStrategy(DiskCacheStrategy.ALL)
                         .into(horizontal_image_item)
+
+                setOnClickListener {
+                    val intent = Intent(context, DetailActivity::class.java)
+                    context.startActivity(intent)
+                }
             }
         }
     }
